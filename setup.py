683dedcefe9fea760db4fac3842cf519153b27a4
umayanga <umayangag@gmail.com>
2021-06-07 14:36:34 +0530

    include setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='gig-python',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Interface to GIG server',
+    author='LDF',
+    license='MIT',
+)
