b5fd53f0b7c1201a4b576dab2469d855a8a0f8c5
umayanga <umayangag@gmail.com>
2022-03-18 20:09:04 +0530

    redefine error types

--- src/gig/gig.py
+++ src/gig/gig.py
@@ -10,13 +10,13 @@ class Server:
         if test_connection == 200:
             print("remote server reached")
         else:
-            raise Exception("Error: unable to reach server")
+            raise SystemError("Error: unable to reach server")
 
     def add(self, entity):
         response = requests.post(self.server_url + f'api/add', json=entity.__dict__)
 
         if response.status_code not in [200, 202]:
-            raise Exception('Entity creation error', json.loads(response.content))
+            raise SystemError('Entity creation error', json.loads(response.content))
         return response.status_code
 
     def add_all(self, entities):
@@ -27,14 +27,14 @@ class Server:
         response = requests.post(self.server_url + f'api/add-batch', json=serialized)
         print(response.status_code)
         if response.status_code not in [200, 202]:
-            raise Exception('Entity creation error', json.loads(response.content))
+            raise SystemError('Entity creation error', json.loads(response.content))
         return response.status_code
 
     def terminate(self, entity):
         response = requests.post(self.server_url + f'api/terminate', json=entity.__dict__)
 
         if response.status_code not in [200, 202]:
-            raise Exception('Entity termination error', json.loads(response.content))
+            raise SystemError('Entity termination error', json.loads(response.content))
         return response.status_code
 
     def get(self, title, date=None, attributes_list=None, image_only=False):
@@ -43,7 +43,7 @@ class Server:
                                 timeout=self.time_out)
 
         if response.status_code != 200:
-            raise Exception('No entity found', json.loads(response.content))
+            raise FileNotFoundError('No entity found', json.loads(response.content))
         return json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
 
     def get_links(self, title, attributes_list=None, page=None, limit=10):
@@ -51,10 +51,10 @@ class Server:
                                 params={"page": page, "attributes": attributes_list, "limit": limit},
                                 timeout=self.time_out)
         if response.status_code != 200:
-            raise Exception('No entity found', json.loads(response.content))
+            raise FileNotFoundError('No entity found', json.loads(response.content))
         result = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
         if result is None:
-            raise Exception('No links found', json.loads(response.content))
+            raise FileNotFoundError('No links found', json.loads(response.content))
         return result
 
     def get_relations(self, title, attributes_list=[], page=1, limit=10):
@@ -62,10 +62,10 @@ class Server:
                                 params={"page": page, "attributes": ','.join(attributes_list), "limit": limit},
                                 timeout=self.time_out)
         if response.status_code != 200:
-            raise Exception('No entity found', json.loads(response.content))
+            raise FileNotFoundError('No entity found', json.loads(response.content))
         result = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
         if result is None:
-            raise Exception('No related entities found')
+            raise FileNotFoundError('No related entities found')
         return result
 
     def search(self, search_text, attributes_list=[], category_list=[], page=1, limit=10):
@@ -74,22 +74,22 @@ class Server:
                                         "categories": ','.join(category_list), "limit": limit},
                                 timeout=self.time_out)
         if response.status_code != 200:
-            raise Exception('No entity found', json.loads(response.content))
+            raise FileNotFoundError('No entity found', json.loads(response.content))
         result = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
         if result is None:
-            raise Exception('No related entities found', json.loads(response.content))
+            raise FileNotFoundError('No related entities found', json.loads(response.content))
         return result
 
     def normalize_name(self, search_text):
         response = requests.get(self.server_url + f'api/normalize/name',
                                 params={"searchText": search_text}, timeout=self.time_out)
         if response.status_code != 200:
-            raise Exception('No result found', json.loads(response.content))
+            raise FileNotFoundError('No result found', json.loads(response.content))
         return json.loads(response.content)
 
     def normalize_location(self, search_text):
         response = requests.get(self.server_url + f'api/normalize/location',
                                 params={"searchText": search_text}, timeout=self.time_out)
         if response.status_code != 200:
-            raise Exception('No result found', json.loads(response.content))
+            raise FileNotFoundError('No result found', json.loads(response.content))
         return json.loads(response.content)
